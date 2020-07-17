661cda1e14b3e3f404951698fcdf115e41e48191
Krista Clarke <kristaclarke82@gmail.com>
2020-07-16 20:42:57 -0700

    created config files, created and populated database

---
+++ Config/orm.js
@@ -0,0 +1,66 @@
+var connection = require('../config/connection.js');
+
+// Helper function for SQL syntax
+function printQuestionMarks(num){
+  var arr = [];
+  for (var i=0; i<num; i++){
+    arr.push('?')
+  }
+  return arr.toString();
+}
+
+// Helper function for SQL syntax
+function objToSql(ob){
+  var arr = [];
+  for (var key in ob) {
+    arr.push(key + '=' + ob[key]);
+  }
+  return arr.toString();
+}
+
+// Object for all SQL functions
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    //vals is an array of values we want to save to cols, cols are the columns to insert the values into
+    // this is creating a new/inserting a new burger
+    create: function(table, cols, vals, cb) {
+      var queryString = 'INSERT INTO ' + table;
+      queryString = queryString + ' (';
+      queryString = queryString + cols.toString();
+      queryString = queryString + ') ';
+      queryString = queryString + 'VALUES (';
+      queryString = queryString + printQuestionMarks(vals.length);
+      queryString = queryString + ') ';
+
+      connection.query(queryString, vals, function(err, result) {
+        if (err) throw err;
+        cb(result);
+      });
+    },
+
+    //objColVals would be columns & values you want to update    ex: {burger name: devour: true}
+    //update existing burger
+    update: function(table, objColVals, condition, cb) {
+      var queryString = 'UPDATE ' + table;
+
+      queryString = queryString + ' SET ';
+      queryString = queryString + objToSql(objColVals);
+      queryString = queryString + ' WHERE ';
+      queryString = queryString + condition;
+
+      console.log(queryString)
+      connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        cb(result);
+      });
+    }
+};
+
+module.exports = orm;
