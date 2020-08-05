2e52c0851bf00b823fa69b1b7c634654f2b08f64
Krista Clarke <kristaclarke82@gmail.com>
2020-08-04 22:23:39 -0700

    changed route to try to deploy to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var path = require("path");
 var app = express();
 
 
@@ -15,7 +15,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+var routes = require(path.join(__dirname, "controllers", "burgers_controller.js"));
 
 app.use(routes);
 
