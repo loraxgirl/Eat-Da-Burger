352784316bfe486af6cd106bb97a0f196062d727
Krista Clarke <kristaclarke82@gmail.com>
2020-09-06 16:55:41 -0700

    updating to deploy to heroku

--- config/orms.js
+++ config/orms.js
@@ -1,4 +1,4 @@
-var connection = require("./connection.js");
+var connection = require("./connections.js");
 
 function printQuestionMarks(num) {
     var arr = [];
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,4 @@
-var orm = require('../config/orm.js');
+var orm = require("../config/orms");
 
 var burger = {
     selectAll: function(cb) {
